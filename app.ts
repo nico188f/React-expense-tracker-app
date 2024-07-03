07d26a8725a2f1101cbe18c7417a7bc4fabc1c07
Nicolaj Blach Jensen <nico188f@gmail.com>
2024-07-03 21:17:26 +0200

    CRUD without the U (update).

---
+++ app.ts
@@ -0,0 +1,13 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { expensesRoute } from "./routes/expenses";
+
+const app = new Hono();
+
+app.use("*", logger());
+
+app.get("/", c => c.text("Hono!"));
+
+app.route("/api/expenses", expensesRoute);
+
+export default app;
